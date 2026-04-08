50e4b5e1fa3d81a179f07deec232a9c4cf86d4b2
JudyPatootie <judithewever@gmail.com>
2026-04-08 22:07:19 +0200

    verwijder 1 categorie

--- rijksmuseum-in-detail_v1/Scripts/category_menu.gd
+++ rijksmuseum-in-detail_v1/Scripts/category_menu.gd
@@ -2,7 +2,6 @@ extends Node2D
 func _ready():
 	$CategoryControlMenu/CenterContainer/CatButtons/Cat1Image/Cat1Button.pressed.connect(func(): start_game("voc"))
 	$CategoryControlMenu/CenterContainer/CatButtons/Cat2Image/Cat2Button.pressed.connect(func(): start_game("bil"))
-	$CategoryControlMenu/CenterContainer/CatButtons/Cat3Image/Cat3Button.pressed.connect(func(): start_game("kind"))
 
 func start_game(category: String):
 	var game_scene = load("res://Scenes/guessing_game.tscn").instantiate()
