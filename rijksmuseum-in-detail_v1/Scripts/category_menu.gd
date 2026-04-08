4c2c93db21a20548a79c2529dfc892c02d5b639c
JudyPatootie <judithewever@gmail.com>
2026-04-08 22:04:53 +0200

    categorieen uitbreiden

--- rijksmuseum-in-detail_v1/Scripts/category_menu.gd
+++ rijksmuseum-in-detail_v1/Scripts/category_menu.gd
@@ -1,8 +1,8 @@
 extends Node2D
 func _ready():
-	$CategoryControlMenu/CenterContainer/CatButtons/Cat1Button.pressed.connect(func(): start_game("voc"))
-	$CategoryControlMenu/CenterContainer/CatButtons/Cat2Button.pressed.connect(func(): start_game("bil"))
-	$CategoryControlMenu/CenterContainer/CatButtons/Cat3Button.pressed.connect(func(): start_game("kind"))
+	$CategoryControlMenu/CenterContainer/CatButtons/Cat1Image/Cat1Button.pressed.connect(func(): start_game("voc"))
+	$CategoryControlMenu/CenterContainer/CatButtons/Cat2Image/Cat2Button.pressed.connect(func(): start_game("bil"))
+	$CategoryControlMenu/CenterContainer/CatButtons/Cat3Image/Cat3Button.pressed.connect(func(): start_game("kind"))
 
 func start_game(category: String):
 	var game_scene = load("res://Scenes/guessing_game.tscn").instantiate()
